b3930d616def3e67ffce9f42c5730e6d4b6648de
Rishabh1220 <118719550+Rishabh1220@users.noreply.github.com>
2023-10-27 00:51:33 +0530

    Update Lecture9-I-CipherSchools.cpp

--- Lecture9-I-CipherSchools.cpp
+++ Lecture9-I-CipherSchools.cpp
@@ -1,1 +1,42 @@
+//functions2
+#include<iostream>
+using namespace std;
 
+void comparision(int a, int b){
+	if (a > b)
+  {
+    cout << "a is greater than b" << endl;
+  }
+  else
+  {
+    cout << "b is greater than a" << endl;
+  }
+}
+
+int main(){
+	int a;
+  int b;
+	cout << "enter the values of a abd b: ";
+  cin>>a>>b;
+	cout<<"Comparision btw two numbers: ";
+  comparision(a,b);
+	return 0;
+}
+
+//functions3
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+	return a+b;
+}
+
+int main(){
+	int a,b;
+	a=1,b=5;
+	int c;
+	c = sum(a,b);
+	cout<<c;
+  
+	return 0;
+}

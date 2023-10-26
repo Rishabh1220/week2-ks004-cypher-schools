0cac9f7bb33000c8783e2fc84d98152ffa8b43ff
Rishabh1220 <118719550+Rishabh1220@users.noreply.github.com>
2023-10-27 00:59:50 +0530

    Update Lecture12-I-CipherSchools.cpp

--- Lecture12-I-CipherSchools.cpp
+++ Lecture12-I-CipherSchools.cpp
@@ -1,1 +1,15 @@
+//Pointers2
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    float b;
+    int *p = &a;
+    cout << "The address of a: " << &a << endl;
+    cout << "The address of b: " << &b << endl;
+    cout << "The value of a: " << *p;
 
+
+    return 0;
+}

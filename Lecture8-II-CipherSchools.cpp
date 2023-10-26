3e5330b567c3a236ba30cada7c0f958d234854b5
Rishabh1220 <118719550+Rishabh1220@users.noreply.github.com>
2023-10-27 00:49:18 +0530

    Update Lecture8-II-CipherSchools.cpp

--- Lecture8-II-CipherSchools.cpp
+++ Lecture8-II-CipherSchools.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+//void printstuff(){
+//	cout<<"writing my first function";
+//}
+
+int sumOfTwoNumber(int a,int b){
+	return a+b;
+}
+
+int main(){
+//	printstuff();
+    int a,b;
+    a=3,b=5;
+    int ans=sumOfTwoNumber(a,b);
+    cout<<ans;
+	return 0;
+}
 
